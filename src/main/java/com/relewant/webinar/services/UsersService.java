b0a690278c4009f28f2c1624894c2d619b4a3477
U0M4737 <luca.fantini@esternibisp.com>
2023-09-14 09:49:04 +0200

    generate xlsx

--- src/main/java/com/relewant/webinar/services/UsersService.java
+++ src/main/java/com/relewant/webinar/services/UsersService.java
@@ -1,8 +1,12 @@
 package com.relewant.webinar.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.relewant.webinar.models.UserDto;
 import com.relewant.webinar.models.UserJsonDto;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +14,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
@@ -26,6 +31,33 @@ public class UsersService {
         
         UserJsonDto userJsonDto = objectMapper.readValue(new File(FILE_PATH + FILE_NAME), UserJsonDto.class);
         log.info("users: {}", userJsonDto);
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Users");
+
+        int rowCount = 0;
+        rowCount = createHeader(sheet, rowCount);
+
+        for(UserDto user : userJsonDto.getUsers()){
+            rowCount = generateRow(sheet, rowCount, user.getFirstName(), user.getLastName(), user.getBirthday());
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(FILE_PATH + "users.xlsx")) {
+            workbook.write(outputStream);
+        }
+        log.info("file excel generato");
         return null;
+    }
+
+    private static int generateRow(XSSFSheet sheet, int rowCount, String field1, String field2, String field3) {
+        Row rowData = sheet.createRow(rowCount++);
+        rowData.createCell(0).setCellValue(field1);
+        rowData.createCell(1).setCellValue(field2);
+        rowData.createCell(2).setCellValue(field3);
+        return rowCount;
+    }
+
+    private static int createHeader(XSSFSheet sheet, int rowCount) {
+        rowCount = generateRow(sheet, rowCount, "FIRST NAME", "LAST NAME", "BIRTHDAY");
+        return rowCount;
     }
 }
